cc4f7b7f2320905fa42dc8b9a0d809aec88e0941
vlad <fordreej@gmail.com>
2021-04-22 15:39:11 +0300

    add method updateStatus in class TaskList

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ class Task {
     id;
     title;
     description;
-    status;
+    statusOne;
+    statusTwo;
+    statusThree;
 
-    constructor (id, title, description) {
+    constructor (id, title, description, statusOne, statusTwo, statusThree) {
         this.id = id;
         this.title = title;
         this.description = description;
-        this.status = enumTaskStatus.Undone;
+        this.statusOne = statusOne;
+        this.statusTwo = statusTwo;
+        this.statusThree = statusThree;
     }
 };
 
@@ -20,11 +24,11 @@ class TaskList {
         this.list = [];
     }
 
-    add(title, description) {
-        let newTask = new Task(this.list.length, title, description);
+    add(title, description, statusOne, statusTwo, statusThree) {
+        let newTask = new Task(this.list.length, title, description, statusOne, statusTwo, statusThree);
         this.list.push(newTask);
         this.renderTask(table, newTask);
-        this.updateStatus();
+        this.updateStatus(select);
     }
 
     renderTask(parent, task) {
@@ -33,25 +37,46 @@ class TaskList {
                                 <td class="cell cell-id">#${task.id + 1}</td>
                                 <td class="cell cell-name">${task.title}</td>
                                 <td class="cell cell-desc">${task.description}</td>
-                                <td class="cell cell-status">${task.status}</td>  
+                                <td class="cell cell-status">
+                                    <select class="select">
+                                        <option value="${task.statusOne}">${task.statusOne}</option>
+                                        <option value="${task.statusTwo}">${task.statusTwo}</option>
+                                        <option value="${task.statusThree}">${task.statusThree}</option>
+                                    </select>
+                                </td>  
                             </tr>
         `;
     }
 
-    updateStatus() {
+    updateStatus(arrItem) {
 
-        const row = document.getElementsByClassName('row');
-        for (let key of row) {
-            console.log(key);
-            break;
+        for (let key of arrItem) {
+            key.addEventListener('change', e => {
+                if (e.target.classList.contains('select')) {
+                    let selectValue = e.target.value;
+
+                    switch (selectValue) {
+                        case 'Done': key.parentElement.parentElement.classList.remove('undone');;
+                                     key.parentElement.parentElement.classList.add('done');
+                                    break;
+                        case 'Undone': key.parentElement.parentElement.classList.remove('done');
+                                       key.parentElement.parentElement.classList.add('undone');
+                                    break;
+                        case 'InProgress': key.parentElement.parentElement.classList.remove('done');
+                                           key.parentElement.parentElement.classList.remove('undone');
+                                    break;
+                    }
+
+                }
+            });
         }
     }
 }
 
 const enumTaskStatus = Object.freeze({
     InProgress: 'InProgress',
-    Undone: 'Undone',
     Done: 'Done',
+    Undone: 'Undone'
 });
 
 let taskList = new TaskList();
@@ -60,9 +85,9 @@ const btnAddTask = document.querySelector('.btn'),
       form = document.getElementById('form'),
       inputName = document.getElementById('name'),
       textArea = document.getElementById('descr'),
-      table = document.querySelector('.tbody');
-      select = document.getElementsByClassName('select');
-    //   row = document.getElementsByClassName('row');
+      table = document.querySelector('.tbody'),
+      select = document.getElementsByClassName('select'),
+      row = document.getElementsByClassName('row');
 
 
 btnAddTask.addEventListener('click', () => {
@@ -71,7 +96,7 @@ btnAddTask.addEventListener('click', () => {
 
 form.addEventListener('submit', item => {
     item.preventDefault();
-    taskList.add(inputName.value, textArea.value);
+    taskList.add(inputName.value, textArea.value, enumTaskStatus.InProgress, enumTaskStatus.Done, enumTaskStatus.Undone);
 
 
     inputName.value = "";
